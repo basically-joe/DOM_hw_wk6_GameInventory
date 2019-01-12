a86a141e4fe881a046f308ee9969eeda20e8fe0b
Joe McColl <joe4kool@gmail.com>
2019-01-12 12:05:36 +0000

    added newGameForm selector and listener to dom, error in console as expected

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("javascript is listening");
+
+  const newGameForm = document.querySelector("#new-game-form");
+  newGameForm.addEventListener("submit", handleNewGameFormSubmit);
 });
