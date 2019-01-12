f0e4d0a23786a84f33d32689e62fcc76b41b51c4
Joe McColl <joe4kool@gmail.com>
2019-01-12 12:25:09 +0000

    returned title to list items, working in browser

--- js/app.js
+++ js/app.js
@@ -6,10 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const handleNewGameFormSubmit = function(event) {
-event.preventDefault();
+  event.preventDefault();
 
-const gameInventoryItem = creategameInventoryItem(event.target);
-const inventoryItem = document.querySelector('#game-inventory');
-inventoryItem.appendChild(gameInventoryItem);
+  const gameInventoryItem = createGameInventoryItem(event.target);
+  const inventoryItem = document.querySelector('#game-inventory');
+  inventoryItem.appendChild(gameInventoryItem);
 
+};
+
+const createGameInventoryItem = function(form) {
+  const gameInventoryItem = document.createElement("li");
+  gameInventoryItem.classList.add("game-inventory-item");
+
+  const title = document.createElement('h2');
+  title.textContent = form.title.value;
+  gameInventoryItem.appendChild(title);
+
+  return gameInventoryItem;
 };
