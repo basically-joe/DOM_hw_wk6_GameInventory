d3d5626c3c21c75769fe1928395078a821e4c00d
Joe McColl <joe4kool@gmail.com>
2019-01-12 12:11:22 +0000

    added handehandleNewGameFormSubmit, error due to no function for createcreateGameInventoryItem as doesn't exist yet

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const newGameForm = document.querySelector("#new-game-form");
   newGameForm.addEventListener("submit", handleNewGameFormSubmit);
 });
+
+const handleNewGameFormSubmit = function(event) {
+event.preventDefault();
+
+const gameInventoryItem = creategameInventoryItem(event.target);
+const inventoryItem = document.querySelector('#game-inventory');
+inventoryItem.appendChild(gameInventoryItem);
+
+};
